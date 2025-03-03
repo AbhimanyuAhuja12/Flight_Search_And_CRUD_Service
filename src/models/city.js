2af937784b66e293084ce1afd4b6aa2c323eefb2
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-03-03 15:08:31 +0530

    added flights model with fk constraint during model definition

--- src/models/city.js
+++ src/models/city.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Airport, {
+      this.hasMany(models.Airport, {
         foreignKey: 'cityId',
         onDelete: 'CASCADE',
         onUpdate:'CASCADE',
