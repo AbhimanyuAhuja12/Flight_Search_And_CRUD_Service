ee4f00b0c28ff4e72b59f5025aaa49c6aa21c4c6
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-03-06 16:37:49 +0530

    Final fix and push

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,4 +2,5 @@ module.exports={
     AirplaneMiddlewares : require('./airplane-middleware.js'),
     CityMiddleware : require('./city-middleware.js'),
     AirportMiddlewares : require('./airport-middlewares.js'),
+    FlightMiddlewares: require('./flight-middlewares.js')
 }
